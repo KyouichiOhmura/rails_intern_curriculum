acb6ad3b4791e4e81775c6a84865fdef3445b22e
Kyouichi Ohmura <kyouichi.ohmura@10xlab.jp>
2013-01-08 17:57:29 +0900

    rspecを試してみる fixuturesファイルを作ってみて データのサイズとデータの中身を確かめるテスト ymlの文法について調べるべき

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,20 @@
 require 'spec_helper'
 
 describe User do
-  pending "add some examples to (or delete) #{__FILE__}"
+	fixtures :users
+
+	before "fixtures test" do
+		@users= User.find(:all)
+	end
+	
+	it "fixture's data size test" do
+		@users.size.should ==3
+	end
+
+	it "test1 data test" do
+		users(:test1).name.should == "test1"
+		users(:test1).user_id.should == 1
+	end
+
+	#pending "add some examples to (or delete) #{__FILE__}"
 end
