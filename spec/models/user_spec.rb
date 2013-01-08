5922c2419d755d71c8ad485b9657945bcefd4e48
Kyouichi Ohmura <kyouichi.ohmura@10xlab.jp>
2013-01-08 18:10:22 +0900

    テストデータすべてをテストに追加

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -15,6 +15,15 @@ describe User do
 		users(:test1).name.should == "test1"
 		users(:test1).user_id.should == 1
 	end
-
+	
+	it "test2 data test" do
+		users(:test2).name.should == "test2"
+		users(:test2).user_id.should == 2
+	end
+	
+	it "test3 data test" do
+		users(:test3).name.should == "test3"
+		users(:test3).user_id.should == 3
+	end
 	#pending "add some examples to (or delete) #{__FILE__}"
 end
