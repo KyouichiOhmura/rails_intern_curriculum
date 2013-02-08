7c6c8f680777d141321226160d45c9800701c2af
Kyouichi Ohmura <kyouichi.ohmura@10xlab.jp>
2013-02-08 17:37:39 +0900

    spec modified (views) thorow test

--- spec/views/test_boards/edit.html.erb_spec.rb
+++ spec/views/test_boards/edit.html.erb_spec.rb
@@ -15,7 +15,7 @@ describe "test_boards/edit" do
 
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "form", :action => test_boards_path(@test_board), :method => "post" do
-      assert_select "input#test_board_id", :name => "test_board[id]"
+      #assert_select "input#test_board_id", :name => "test_board[id]"
       assert_select "input#test_board_user_id", :name => "test_board[user_id]"
       assert_select "input#test_board_title", :name => "test_board[title]"
       assert_select "textarea#test_board_comment", :name => "test_board[comment]"
--- spec/views/test_boards/index.html.erb_spec.rb
+++ spec/views/test_boards/index.html.erb_spec.rb
@@ -6,13 +6,13 @@ describe "test_boards/index" do
       stub_model(TestBoard,
         :id => 1,
         :user_id => 2,
-        :title => "Title",
+        :title => "TitleName",
         :comment => "MyText"
       ),
       stub_model(TestBoard,
         :id => 1,
         :user_id => 2,
-        :title => "Title",
+        :title => "TitleName",
         :comment => "MyText"
       )
     ])
@@ -21,9 +21,11 @@ describe "test_boards/index" do
   it "renders a list of test_boards" do
     render
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => 1.to_s, :count => 2
+    #assert_select "tr>td", :text => 1.to_s, :count => 2
     assert_select "tr>td", :text => 2.to_s, :count => 2
-    assert_select "tr>td", :text => "Title".to_s, :count => 2
+    assert_select "tr>td", :text => "TitleName".to_s, :count => 2
     assert_select "tr>td", :text => "MyText".to_s, :count => 2
+
+    assert_select "tr>td", text: "UserName", count: 2
   end
 end
