6632b68496e2553f12c14bb8464b0b13c2353d6d
Kyouichi Ohmura <kyouichi.ohmura@10xlab.jp>
2013-02-15 16:36:27 +0900

    unique設定の追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :user_id
 
-  validates :name, :presence=>true
-  validates :user_id, :presence=>true
-
+  validates :name, :presence=>true, :uniqueness=>true
+  validates :user_id, :presence=>true, :uniqueness=>true
 
 end
