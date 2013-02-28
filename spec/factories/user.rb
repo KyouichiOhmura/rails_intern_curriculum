65c78d077a4e39a049662823a1c6dd1ef5cd3cf9
Kyouichi Ohmura <kyouichi.ohmura@10xlab.jp>
2013-02-28 19:27:55 +0900

    pass spec

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :user, class: User do
-    user_id 123
+    user_id 1
     name "hoge"
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,7 +6,7 @@ describe User do
   before "fixtures test" do
     @users= User.find(:all)
   end
-	
+
   it "fixture's data size test" do
     @users.size.should ==4
   end
@@ -15,12 +15,12 @@ describe User do
     users(:test1).name.should == "test1"
     users(:test1).user_id.should == 1
   end
-	
+
   it "test2 data test" do
     users(:test2).name.should == "test2"
     users(:test2).user_id.should == 2
   end
-	
+
   it "test3 data test" do
     users(:test3).name.should == "test3"
     users(:test3).user_id.should == 3
